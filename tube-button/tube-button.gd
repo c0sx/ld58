0f0a7be227fbdb302c3191adb1aae17d175565b3
Koshkin Nikolai <c0sx@users.noreply.github.com>
2025-10-25 15:54:21 +0400

    fix: migration errors

--- tube-button/tube-button.gd
+++ tube-button/tube-button.gd
@@ -16,7 +16,6 @@ func _ready() -> void:
 	_area3d.body_entered.connect(_on_enter)
 	_area3d.body_exited.connect(_on_exit)
 	_animation_player.animation_finished.connect(_on_animation_finished)
-
 	_cursor.visible = false
 
 func interact() -> void:
